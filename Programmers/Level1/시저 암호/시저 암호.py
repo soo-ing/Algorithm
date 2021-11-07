c41d5b70a9a6e00e03f9c79c68741db35951441b
soo-ing <sooing5412@naver.com>
2021-11-07 19:56:18 +0900

    commit

---
+++ Programmers/Level1/시저 암호/시저 암호.py
@@ -0,0 +1,14 @@
+def solution(s, n):
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i] = chr((ord(s[i]) - ord('A') + n) % 26 + ord('A'))
+        elif s[i].islower():
+            s[i] = chr((ord(s[i]) - ord('a') + n) % 26 + ord('a'))
+
+    return "".join(s)
+
+"""
+chr() : 아스키코드를 문자로
+ord() : 문자를 아스키코드로
+"""
